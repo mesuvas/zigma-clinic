faec37385e905d1555b5bb4cd15dc63e1d65eded
mesuvas <suvas.magar.69@gmail.com>
2025-03-21 21:11:23 +0545

    modified billing module

--- backend/models/billingModel.ts
+++ backend/models/billingModel.ts
@@ -1,33 +1,30 @@
 import mongoose from "mongoose";
 
 const billingSchema = new mongoose.Schema({
-    
-    patientID: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        description: "Must be a string and is required"
-    },
-    amount: {
-        type: Number,
-        required: true,
-        description: "Must be a number and is required"
-    },
-    paymentStatus: {
-        type: String,
-        enum: ["paid", "unpaid"],
-        required: true,
-        description: "Must be a string and is required"
-    },
-    billingDate: {
-        type: Date,
-        required: true,
-        description: "Must be a date and is required"
-    }
+  patientID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    description: "Must be a string and is required",
+  },
+
+  amount: {
+    type: Number,
+    required: true,
+    description: "Must be a number and is required",
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["paid", "unpaid"],
+    required: true,
+    description: "Must be a string and is required",
+  },
+  billingDate: {
+    type: Date,
+    required: true,
+    description: "Must be a date and is required",
+  },
 });
 
-const billingModel= mongoose.model("billings",billingSchema);
+const billingModel = mongoose.model("billings", billingSchema);
 
 export default billingModel;
-
-
-
